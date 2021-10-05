4869c0400c30f639eadf1e6622dbd479207fbdb3
Purvi Patel <91878359+purvi1999@users.noreply.github.com>
2021-10-05 21:06:32 +0530

    Add files via upload

---
+++ Practical-2.js
@@ -0,0 +1,6 @@
+const os=require("os");
+console.log("Os CPU architecture : ",os.arch());
+console.log("Free memory of the System : ",os.freemem());
+console.log("Total memory of the System : ",os.totalmem());
+console.log("Os platform : ",os.platform());
+console.log("Information about the current user : ",os.userInfo());
